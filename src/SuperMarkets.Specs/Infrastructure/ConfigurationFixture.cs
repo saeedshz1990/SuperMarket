bd8bf9c6546bae1182b73273a520daa79b69fba5
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2022-05-02 02:35:04 +0430

    Create Specs layerTest

---
+++ src/SuperMarkets.Specs/Infrastructure/ConfigurationFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SuperMarket.Persistence.EF;
+using Xunit;
+
+namespace SuperMarkets.Specs.Infrastructure
+{
+    public class ConfigurationFixture
+    {
+        public TestSettings Value { get; private set; }
+
+        public ConfigurationFixture()
+        {
+            Value = GetSettings();
+        }
+
+        private TestSettings GetSettings()
+        {
+            var settings = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .AddCommandLine(Environment.GetCommandLineArgs())
+                .Build();
+
+            var testSettings = new TestSettings();
+            settings.Bind(testSettings);
+            return testSettings;
+        }
+    }
+
+    public class TestSettings
+    {
+        public DbContextOptions<EFDataContext> DbConnectionString { get; set; }
+    }
+
+    [CollectionDefinition(nameof(ConfigurationFixture), DisableParallelization = false)]
+    public class ConfigurationCollectionFixture : ICollectionFixture<ConfigurationFixture>
+    {
+    }
+}
---
+++ src/SuperMarkets.Specs/Infrastructure/EFDataContextDatabaseFixture.cs
@@ -0,0 +1,21 @@
+﻿using SuperMarket.Persistence.EF;
+using Xunit;
+
+namespace SuperMarkets.Specs.Infrastructure
+{
+    [Collection(nameof(ConfigurationFixture))]
+    public class EFDataContextDatabaseFixture : DatabaseFixture
+    {
+        readonly ConfigurationFixture _configuration;
+
+        public EFDataContextDatabaseFixture(ConfigurationFixture configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public EFDataContext CreateDataContext()
+        {
+            return new EFDataContext(_configuration.Value.DbConnectionString);
+        }
+    }
+}
